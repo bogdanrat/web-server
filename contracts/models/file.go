a4f8cb6ab5e7dc9115e3dfbb62727eeef662b110
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-05-28 23:01:24 +0300

    Changed: GetFilesResponse

--- contracts/models/file.go
+++ contracts/models/file.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type GetFilesResponse struct {
-	Key          string    `json:"key"`
-	LastModified time.Time `json:"last_modified"`
-	Size         uint64    `json:"size"`
-	StorageClass string    `json:"storage_class"`
+	Key          string     `json:"key"`
+	LastModified *time.Time `json:"last_modified,omitempty"`
+	Size         uint64     `json:"size"`
+	StorageClass string     `json:"storage_class"`
 }
