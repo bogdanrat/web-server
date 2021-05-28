5b87e9a99f0c44911fab32706f026cb51d37f47f
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-05-28 22:51:40 +0300

    Added: GetFilesResponse

---
+++ contracts/models/file.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type GetFilesResponse struct {
+	Key          string    `json:"key"`
+	LastModified time.Time `json:"last_modified"`
+	Size         uint64    `json:"size"`
+	StorageClass string    `json:"storage_class"`
+}
