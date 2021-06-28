3aac4432826ad0801c11ce04ed62b5b45b2da876
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-06-28 23:31:04 +0300

    Removed: SQS Event Emitter

--- service/queue/go.mod
+++ service/queue/go.mod
@@ -3,8 +3,9 @@ module github.com/bogdanrat/web-server/service/queue
 go 1.16
 
 require (
-	github.com/bogdanrat/web-server/contracts v0.0.0-20210609063922-20041c59a2a1
-	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/aws/aws-sdk-go v1.38.69
+	github.com/bogdanrat/web-server/contracts v0.0.0-20210628193136-bfcbabf54276
+	github.com/mitchellh/mapstructure v1.4.1
 	github.com/streadway/amqp v1.0.0
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
 )
---
+++ service/queue/sqs/emitter.go
@@ -0,0 +1,114 @@
+package sqs
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/bogdanrat/web-server/service/queue"
+	"log"
+)
+
+type Config struct {
+	QueueName                 string
+	FifoQueue                 string
+	ContentBasedDeduplication string
+	DelaySeconds              string
+	MessageRetentionPeriod    string
+}
+
+type sqsEventEmitter struct {
+	svc      *sqs.SQS
+	queueUrl *string
+	isFifo   bool
+}
+
+func NewEventEmitter(sess *session.Session, config Config) (queue.EventEmitter, error) {
+	if sess == nil {
+		return nil, fmt.Errorf("aws session is nil")
+	}
+
+	svc := sqs.New(sess)
+	emitter := &sqsEventEmitter{
+		svc: svc,
+	}
+
+	if err := emitter.setup(config); err != nil {
+		return nil, err
+	}
+	return emitter, nil
+}
+
+func (e *sqsEventEmitter) setup(config Config) error {
+	output, err := e.svc.GetQueueUrl(&sqs.GetQueueUrlInput{
+		QueueName: aws.String(config.QueueName),
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			code := aerr.Code()
+			switch code {
+			case sqs.ErrCodeQueueDoesNotExist, "NotFound":
+				if err := e.createQueue(config); err != nil {
+					return err
+				}
+				log.Printf("SQS Queue initialized: %s\n", *e.queueUrl)
+				return nil
+			}
+		}
+		return err
+	}
+	e.queueUrl = output.QueueUrl
+
+	return nil
+}
+
+func (e *sqsEventEmitter) createQueue(config Config) error {
+	output, err := e.svc.CreateQueue(&sqs.CreateQueueInput{
+		QueueName: aws.String(config.QueueName),
+		Attributes: map[string]*string{
+			"FifoQueue":                 aws.String(config.FifoQueue),
+			"ContentBasedDeduplication": aws.String(config.ContentBasedDeduplication),
+			"DelaySeconds":              aws.String(config.DelaySeconds),
+			"MessageRetentionPeriod":    aws.String(config.MessageRetentionPeriod),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	e.queueUrl = output.QueueUrl
+	e.isFifo = true
+	return nil
+}
+
+func (e *sqsEventEmitter) Emit(event queue.Event) error {
+	jsonBody, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("could not marshal json event: %s", err.Error())
+	}
+
+	var messageGroupID *string
+	switch event.Name() {
+	case "userSignUp":
+		messageGroupID = aws.String("auth")
+	}
+
+	message := &sqs.SendMessageInput{
+		QueueUrl:    e.queueUrl,
+		MessageBody: aws.String(string(jsonBody)),
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"event_name": {
+				DataType:    aws.String("string"),
+				StringValue: aws.String(event.Name()),
+			},
+		},
+	}
+	if e.isFifo {
+		message.MessageGroupId = messageGroupID
+	}
+
+	_, err = e.svc.SendMessage(message)
+	return err
+}
