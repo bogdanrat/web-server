05ae1a5de2def029ab7e8cccfa4c28176add3eaf
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-07-23 08:18:31 +0300

    port & build script update

--- build.sh
+++ build.sh
@@ -1,15 +1,21 @@
 echo 'Building images...'
 
-docker image build --build-arg ACCESS_TOKEN_USR=$ACCESS_TOKEN_USR --build-arg ACCESS_TOKEN_PWD=$ACCESS_TOKEN_PWD -t bogdanrat/webserver_coreservice ./service/core
-docker image build --build-arg ACCESS_TOKEN_USR=$ACCESS_TOKEN_USR --build-arg ACCESS_TOKEN_PWD=$ACCESS_TOKEN_PWD -t bogdanrat/webserver_authservice ./service/auth
-docker image build --build-arg ACCESS_TOKEN_USR=$ACCESS_TOKEN_USR --build-arg ACCESS_TOKEN_PWD=$ACCESS_TOKEN_PWD -t bogdanrat/webserver_storageservice ./service/storage
+GIT_COMMIT_SHA=$(git rev-parse HEAD)
+
+docker image build --build-arg ACCESS_TOKEN_USR=$ACCESS_TOKEN_USR --build-arg ACCESS_TOKEN_PWD=$ACCESS_TOKEN_PWD -t bogdanrat/webserver_coreservice:latest -t bogdanrat/webserver_coreservice:$GIT_COMMIT_SHA ./service/core
+docker image build --build-arg ACCESS_TOKEN_USR=$ACCESS_TOKEN_USR --build-arg ACCESS_TOKEN_PWD=$ACCESS_TOKEN_PWD -t bogdanrat/webserver_authservicelatest -t bogdanrat/webserver_authservice:$GIT_COMMIT_SHA ./service/auth
+docker image build --build-arg ACCESS_TOKEN_USR=$ACCESS_TOKEN_USR --build-arg ACCESS_TOKEN_PWD=$ACCESS_TOKEN_PWD -t bogdanrat/webserver_storageservice:latest -t bogdanrat/webserver_storageservice:$GIT_COMMIT_SHA ./service/storage
 
 echo 'Pushing images...'
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
 
-docker push bogdanrat/webserver_coreservice
-docker push bogdanrat/webserver_authservice
-docker push bogdanrat/webserver_storageservice
+docker push bogdanrat/webserver_coreservice:latest
+docker push bogdanrat/webserver_coreservice:$GIT_COMMIT_SHA
+
+docker push bogdanrat/webserver_authservice:latest
+docker push bogdanrat/webserver_authservice:$GIT_COMMIT_SHA
+
+docker push bogdanrat/webserver_storageservice:latest
+docker push bogdanrat/webserver_storageservice:$GIT_COMMIT_SHA
 
 echo 'Done.'
-
