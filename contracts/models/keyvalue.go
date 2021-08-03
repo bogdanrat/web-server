94b36bccd501989781e607359c9d6be9aff77113
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-08-03 19:04:09 +0300

    Added: models

--- contracts/models/keyvalue.go
+++ contracts/models/keyvalue.go
@@ -4,3 +4,19 @@ type KeyValuePair struct {
 	Key   string
 	Value interface{}
 }
+
+type GetPairRequest struct {
+	Key string `form:"key"`
+}
+
+type PutPairRequest struct {
+	Key   string `form:"key"`
+	Value string `form:"value"`
+}
+
+type DeletePairRequest struct {
+	Key string `form:"key"`
+}
+
+type GetAllPairsRequest struct {
+}
